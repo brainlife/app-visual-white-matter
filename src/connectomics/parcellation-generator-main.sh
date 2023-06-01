fe8f1cb2d6a122ec2b3ce9c49c7532836ede1fb3
Brad Caron <bacaron@utexas.edu>
2023-06-01 13:34:54 -0500

    reason: small fix

--- src/connectomics/parcellation-generator-main.sh
+++ src/connectomics/parcellation-generator-main.sh
@@ -4,8 +4,8 @@
 #visual-white-matter-connectomics-parcellation-generator
 
 # create niftis of eccentricity parcellations before final merge
-# echo $FREESURFER_LICENSE > license.txt
-cat $FREESURFER_LICENSE > license.txt
+echo $FREESURFER_LICENSE > license.txt
+# cat $FREESURFER_LICENSE > license.txt
 time singularity exec -e -B `pwd`/license.txt:/usr/local/freesurfer/license.txt docker://brainlife/connectome_workbench:1.4.2-freesurfer-update ./src/connectomics/parcellation-generator/polar-angle-by-eccentricity-nifti-generator.sh
 
 # create final parcellation
