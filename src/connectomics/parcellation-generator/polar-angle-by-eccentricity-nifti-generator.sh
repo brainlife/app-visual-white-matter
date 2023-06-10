18eb0735972c241ef04a19a99a6187d373a1ba24
bacaron <bacaron245@gmail.com>
2023-06-10 03:16:36 -0500

    reason: small fix

--- src/connectomics/parcellation-generator/polar-angle-by-eccentricity-nifti-generator.sh
+++ src/connectomics/parcellation-generator/polar-angle-by-eccentricity-nifti-generator.sh
@@ -86,7 +86,7 @@ minDegreePA=($minDegreePA)
 maxDegreePA=($maxDegreePA)
 minDegreeECC=($minDegreeECC)
 maxDegreeECC=($maxDegreeECC)
-echo $maxDegreeECC
+echo ${maxDegreeECC[*]}
 
 # set up some stuff to move inputaparc to space
 if [ -f ${dwi} ]; then
