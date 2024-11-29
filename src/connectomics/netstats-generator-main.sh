86f9d63fea8ea08968780ef32419833c06c7e430
Junbeom Kwon <76743210+Junebeomstics@users.noreply.github.com>
2024-11-29 11:07:24 -0600

    Update netstats-generator-main.sh
    added --no-home to solve compatibility error

--- src/connectomics/netstats-generator-main.sh
+++ src/connectomics/netstats-generator-main.sh
@@ -4,4 +4,4 @@ set -xe
 #nodes=1:ppn=2,walltime=1:00:00
 #visual-white-matter-connectomics-netstats-generator
 
-time singularity exec -e docker://brainlife/ga-python:lab328-dipy141-pybrainlife-1.0 ./src/connectomics/netstats-generator/generate-netstats.py
+time singularity exec --no-home -e docker://brainlife/ga-python:lab328-dipy141-pybrainlife-1.0 ./src/connectomics/netstats-generator/generate-netstats.py
