de07760c6ae56954fdcf27f23f66e6de062c2bac
JB <kjb961013@snu.ac.kr>
2024-12-04 23:22:41 -0600

    update network-generator and network-measurements

--- src/connectomics/network-generator-main.sh
+++ src/connectomics/network-generator-main.sh
@@ -11,39 +11,39 @@ SINGULARITYENV_PYTHONNOUSERSITE=true
 conmats="count density length denlen"
 
 
-files=(`find ./connectomes/track* -maxdepth 0 -type d`)
+# files=(`find ./connectomes/track* -maxdepth 0 -type d`)
 
-for j in ${files[*]}
+# for j in ${files[*]}
+# do
+# grab base name
+bname=track
+for i in ${conmats}
 do
-    # grab base name
-    bname=${j##./connectomes/}
-    for i in ${conmats}
-    do
-        fstem=./connectomes/${bname}/${i}_out/
-        
-        # grab label
-        label=${fstem}/label.json
+    fstem=./connectomes/${bname}/${i}_out/
+    
+    # grab label
+    label=${fstem}/label.json
 
-        # grab correlation
-        correlation=${fstem}/csv/
+    # grab correlation
+    correlation=${fstem}/csv/
 
-        # grab index
-        index=${fstem}/index.json
+    # grab index
+    index=${fstem}/index.json
 
-        # create temporary config.json
-        cat << EOF > tmp.json
+    # create temporary config.json
+    cat << EOF > tmp.json
 {
-    "index": "${index}",
-    "label": "${label}",
-    "csv": "${correlation}"
+"index": "${index}",
+"label": "${label}",
+"csv": "${correlation}"
 }
 EOF
-        echo $bname $i
-        [ ! -f ./networks/${bname}/${i}/network.json.gz ] && singularity exec -e docker://filsilva/cxnetwork:0.2.0 ./src/connectomics/network-generator/network-generator.py tmp.json ./networks/${bname}/${i}
+    echo $bname $i
+    [ ! -f ./networks/${bname}/${i}/network.json.gz ] && singularity exec -e docker://filsilva/cxnetwork:0.2.0 ./src/connectomics/network-generator/network-generator.py tmp.json ./networks/${bname}/${i}
 
-        rm -rf tmp.json
-    done
+    rm -rf tmp.json
 done
+# done
 echo "done"
 
 
--- src/connectomics/network-measurements-main.sh
+++ src/connectomics/network-measurements-main.sh
@@ -3,34 +3,34 @@ set -xe
 #nodes=1:ppn=1,vmem=6gb,walltime=01:00:00
 #visual-white-matter-connectomics-network-measurements
 
-files=(`find ./networks/track* -maxdepth 0 -type d`)
+# files=(`find ./networks/track* -maxdepth 0 -type d`)
 conmats="count density length denlen"
 richClubPercentage=`jq -r '.richClubPercentage' config.json`
 
-for j in ${files[*]}
-do
+# for j in ${files[*]}
+# do
     # grab base name
-    bname=${j##./networks/}
-    for i in ${conmats}
-    do
-        fstem=./networks/${bname}/${i}/
-        
-        # network
-        network=${fstem}/network.json.gz
+bname=track #${j##./networks/}
+for i in ${conmats}
+do
+    fstem=./networks/${bname}/${i}/
+    
+    # network
+    network=${fstem}/network.json.gz
 
-        # create temporary config.json
-        cat << EOF > tmp.json
+    # create temporary config.json
+    cat << EOF > tmp.json
 {
-    "network": "${network}",
-    "richClubPercentage": "${richClubPercentage}"
+"network": "${network}",
+"richClubPercentage": "${richClubPercentage}"
 }
 EOF
-        echo $bname $i
-        [ ! -f ./networks/${bname}/${i}/measurements/network.json.gz ] && singularity exec -e docker://filsilva/cxnetwork:0.2.0 ./src/connectomics/network-measurements/network-measurements.py tmp.json ./networks/${bname}/${i}/measurements
+    echo $bname $i
+    [ ! -f ./networks/${bname}/${i}/measurements/network.json.gz ] && singularity exec -e docker://filsilva/cxnetwork:0.2.0 ./src/connectomics/network-measurements/network-measurements.py tmp.json ./networks/${bname}/${i}/measurements
 
-        rm -rf tmp.json
-    done
+    rm -rf tmp.json
 done
+# done
 
 # clean up file names
 singularity exec -e docker://filsilva/cxnetwork:0.2.0 ./src/connectomics/network-measurements/clean-up-file-names.py
